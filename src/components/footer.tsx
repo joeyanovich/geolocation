25eab50d91cf3328dc9bfa19fdbe04ed9eb213e7
joeyanovich <joedison.dias@gmail.com>
2024-10-03 19:26:51 -0300

    feat: create footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,19 @@
 export function Footer() {
   return (
     <>
-      <div>Footer</div>
+      <div className="fixed bottom-0 flex items-center justify-start h-28 w-full px-40 text-slate-900 bg-slate-300 opacity-80">
+        <div className="flex items-center justify-center w-full">
+          <p className="font-bold text-center text-sm w-full text-slate-700">
+            Feito com muito <span className="text-red-400">♥</span> por{" "}
+            <a
+              href="https://github.com/joeyanovich"
+              className="underline text-slate-900"
+            >
+              Joey Dias
+            </a>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
