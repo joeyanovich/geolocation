07d138e3c01fc5d4963410fc03ab52466ea2eb64
joeyanovich <joedison.dias@gmail.com>
2024-10-03 16:59:37 -0300

    feat: create header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <>
-      <div>header</div>
-    </>
+    <div className="flex items-center justify-center h-20 w-full px-40 bg-slate-300 opacity-80">
+      <h1 className="text-3xl w-full">Geolocalização</h1>
+    </div>
   );
 }
