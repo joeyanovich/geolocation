0a96eb1a14ff894a7dd0ac7d55e5fc50162e0c4e
joeyanovich <joedison.dias@gmail.com>
2024-10-03 19:43:31 -0300

    feat: adjust responsive sizes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
     <>
-      <div className="fixed bottom-0 flex items-center justify-start h-28 w-full px-40 text-slate-900 bg-slate-300 opacity-80">
+      <div className="fixed bottom-0 flex items-center justify-start h-16 sm:h-28 w-full px-5 lg:px-40 text-slate-900 bg-slate-300 opacity-80">
         <div className="flex items-center justify-center w-full">
-          <p className="font-bold text-center text-sm w-full text-slate-700">
+          <p className="font-bold text-center text-xs sm:text-sm w-full text-slate-700">
             Feito com muito <span className="text-red-400">♥</span> por{" "}
             <a
               href="https://github.com/joeyanovich"
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,14 @@ import { MapPinArea } from "@phosphor-icons/react";
 
 export function Header() {
   return (
-    <div className="fixed top-0 flex items-center justify-start h-28 w-full px-40 text-slate-900 bg-slate-300 opacity-80">
-      <div className="flex items-center gap-2">
-        <MapPinArea weight="fill" className="text-8xl" />
-        <h1 className="font-extrabold text-3xl w-full text-slate-700">
-          <span className=" text-slate-900">Geo</span>localização
+    <div className="fixed top-0 flex items-center justify-start h-16 sm:h-28 w-full px-5 lg:px-40 text-slate-900 bg-slate-300 opacity-80">
+      <div className="flex items-center gap-2 max-w-full">
+        <MapPinArea
+          weight="fill"
+          className="text-3xl sm:text-5xl lg:text-8xl"
+        />
+        <h1 className="font-extrabold sm:text-2xl lg:text-3xl w-full text-slate-700">
+          <span className="text-slate-900">Geo</span>localização
         </h1>
       </div>
     </div>
