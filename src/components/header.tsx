2a78946537e148ff4e22c3e2537f460f819eb4e2
joeyanovich <joedison.dias@gmail.com>
2024-10-03 19:10:53 -0300

    feat: add font and costumize header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import { MapPinArea } from "@phosphor-icons/react";
+
 export function Header() {
   return (
-    <div className="flex items-center justify-center h-20 w-full px-40 bg-slate-300 opacity-80">
-      <h1 className="text-3xl w-full">Geolocalização</h1>
+    <div className="fixed top-0 flex items-center justify-start h-28 w-full px-40 text-slate-900 bg-slate-300 opacity-80">
+      <div className="flex items-center gap-2">
+        <MapPinArea weight="fill" className="text-8xl" />
+        <h1 className="font-extrabold text-3xl w-full text-slate-700">
+          <span className=" text-slate-900">Geo</span>localização
+        </h1>
+      </div>
     </div>
   );
 }
